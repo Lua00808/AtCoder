a2db7525c6045c43bf58f4b4cc899a77e077a51d
Lua00808 <3102oikawa713hayaki@gmail.com>
2020-06-13 23:56:11 +0900

    東京上海日動 プログラミングコンテスト2020 解答を追加

---
+++ TMN2020/B.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a, b, v, w,t int
+	var D int
+	fmt.Scan(&a, &v)
+	fmt.Scan(&b, &w)
+	fmt.Scan(&t)
+
+	if a > b {
+		D = a-b
+	} else {
+		D= b-a
+	}
+	D2:=(v-w)*t
+	if D2>=D {
+		fmt.Println("YES")
+		return
+	}
+	fmt.Println("NO")
+}
